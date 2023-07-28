6aaa4a6219c3b370834d628138b1c572c9bce7fb
Nyumamkamanga <nyumamkamanga1@gmail.com>
2023-07-27 18:17:48 -0700

    Addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,53 +1,78 @@
 #include "main.h"
 
 /**
+ * rev_string - reverse array
+ * @n: Integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i;)
+	j++;
+	i--;
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
  * infinite_add - adds two numbers
- * @n1: First number
- * @n2: Second number
- * @r: Result
- * @size_r: Sesult length
- * Return: Sum
+ * @n1: First number to add
+ * @n2: Second number to add
+ * @r: Pointer to the buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	/* local variable declaration */
-	int 1 = 0, j = 0, k, l = 0, f, s, d = 0;
+	int overflow = 0, i = 0, j = 0, digitd = 0;
+	int val1 = 0, val2 = 0, temp_to = 0;
 
-	while (n1[i] != '\0')
+	while (*(n1 + i) != '\0')
 		i++;
-	while (n2[j] != '\0')
+	while (*(n2 + j) != '\0')
 		j++;
-	if (i > j)
-		l = i;
-	else
-		l = j;
-	if (l + 1 > size_r)
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	r[1] = '\0';
-	for (k = l - 1 ; k >= 0 ; k--)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		i--;
-		j--;
-		if (i >= 0)
-			f = n1[i] - '0';
+		if (i < 0)
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
 		else
-			f = 0;
-		if (j >= 0)
-			s = n2[j] - '0';
+			val2 = *(n2 + j) - '0';
+		temp_to = val1 + val2 + overflow;
+		if (temp_to >= 10)
+			overflow = 1;
 		else
-			s = 0;
-		r[k] = (f + s + d) % 10 + '0';
-		d = (f + s + d) / 10;
-	}
-	if (d == 1)
-	{
-		r[l + 1] = '\0';
-		if (l + 2 > size_r)
+			overflow = 0;
+		if (digitd >= (size_r - 1))
 			return (0);
-		while (1-- >= 0)
-			r[l + 1] = r[l];
-		r[0] = d + '0';
+		*(r + digitd) = (temp_to % 10) - '0';
+		digitd++;
+		j--;
+		i--;
 	}
+	if (digitd == size_r)
+		return (0);
+	*(r + digitd) = '0';
+	rev_string(r);
 	return (r);
 }
