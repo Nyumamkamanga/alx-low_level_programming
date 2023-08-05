3e713cd6a8a7cd9c7fff673568c5b320a5ca1d3a
Nyumamkamanga <nyumamkamanga1@gmail.com>
2023-08-05 02:18:03 -0700

    updated

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: The character is to check
+ * Return: 1 if c lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+
+{
+	return (c >= 'a' && c <= 'z');
+}
